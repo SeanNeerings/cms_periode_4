edd9501298ad4175510b12e1c52af93aa2fe4f05
SeanNeerings <86488785+SeanNeerings@users.noreply.github.com>
2022-06-03 14:45:41 +0200

    Add files via upload

---
+++ cmsp4/functions.php
@@ -0,0 +1,24 @@
+<?php
+  function arphabet_widgets_init() {
+
+    register_sidebar( array(
+      'name'          => 'Sidebar',
+      'id'            => 'sidebar-widgets',
+      'before_widget' => '<div class="side-widget">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3 class="rounded">',
+      'after_title'   => '</h3>',
+    ) );
+    
+    }
+    
+    add_action( 'widgets_init', 'arphabet_widgets_init' );
+
+
+
+    
+    function mytheme_menu(){
+      register_nav_menu('mijn_custom_menu', __( 'Mijn Custom Menu' ));
+     }
+     add_action('init', 'mytheme_menu');
+?>
